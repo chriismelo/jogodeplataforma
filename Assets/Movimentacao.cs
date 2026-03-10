7d05483c2cd832e9a80596b1aa43267ac9156298
chriismelo <christyannemelo.senai25@gmail.com>
2026-03-10 11:38:28 -0300

    ultima modificacao

--- Assets/Movimentacao.cs
+++ Assets/Movimentacao.cs
@@ -5,6 +5,7 @@ public class Movimentacao : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        frameAtual = frameAtual + 1;
     }
     // Update is called once per frame
     void Update()
@@ -16,5 +17,18 @@ public class Movimentacao : MonoBehaviour
           transform.Translate(0.05f, 0, 0);
 
 
-        Physics.CheckSphere(transform.position, 1f);
+       if (Input.GetKey("a")) {
+            this.transform.Translate(-0.05, 0, 0);
+
+            if (Input.GetKey("space") && pulando == false && Physics.CheckSphere(transform.position, 2f))
+                pulando = true;
+            inicioPulo = frameAtual;
+
+            if (pulando && frameAtual - inicioPulo > 100)
+                pulando = false;
 }
+        if (pulando)
+        {
+            this.transform.Translate(0, 0.05, 0);
+
+        }
